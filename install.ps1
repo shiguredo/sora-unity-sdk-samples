461367cd79cb347d54fe8b30101bead1cae17a33
torikizi <kizifrog@gmail.com>
2020-07-25 14:39:07 +0900

    Sora Unity SDK 2020.5 を利用する - Sora Unity SDK 2020.5 を利用するように修正しました

--- install.ps1
+++ install.ps1
@@ -3,7 +3,7 @@
 $ErrorActionPreference = 'Stop'
 
 
-$SORAUNITYSDK_VERSION = "2020.4"
+$SORAUNITYSDK_VERSION = "2020.5"
 
 # 一通り掃除
 if (Test-Path "SoraUnitySdkSamples\Assets\Plugins\SoraUnitySdk") {
