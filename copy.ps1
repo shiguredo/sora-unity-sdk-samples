dac65b18e2d5db1945554177c35cd9d9a1a470a3
melpon <shigemasa7watanabe+github@gmail.com>
2020-08-05 11:08:44 +0900

    iOS 用の設定を追加

--- copy.ps1
+++ copy.ps1
@@ -1,5 +1,7 @@
 # 開発用スクリプト。 ..\sora-unity-sdk にあるソースと dll をこのプロジェクトにコピーする
 mkdir SoraUnitySdkSamples\Assets\Plugins\SoraUnitySdk\windows\x86_64 -Force
-mkdir SoraUnitySdkSamples\Assets\SoraUnitySdk -Force
-Copy-Item ..\sora-unity-sdk\Sora\Sora.cs SoraUnitySdkSamples\Assets\SoraUnitySdk\
+if (Test-Path "SoraUnitySdkSamples\Assets\SoraUnitySdk") {
+    Remove-Item "SoraUnitySdkSamples\Assets\SoraUnitySdk" -Force -Recurse
+}
+Copy-Item -Recurse ..\sora-unity-sdk\Sora\ SoraUnitySdkSamples\Assets\SoraUnitySdk\
 Copy-Item ..\sora-unity-sdk\build\Release\SoraUnitySdk.dll SoraUnitySdkSamples\Assets\Plugins\SoraUnitySdk\windows\x86_64\
--- copy.sh
+++ copy.sh
@@ -1,17 +1,24 @@
 # 開発用スクリプト。 ..\sora-unity-sdk にあるソースと bundle をこのプロジェクトにコピーする
 mkdir -p SoraUnitySdkSamples/Assets/SoraUnitySdk
 cp ../sora-unity-sdk/Sora/Sora.cs SoraUnitySdkSamples/Assets/SoraUnitySdk
+rm -rf SoraUnitySdkSamples/Assets/SoraUnitySdk/Editor/
+cp -r ../sora-unity-sdk/Sora/Editor/ SoraUnitySdkSamples/Assets/SoraUnitySdk/Editor/
 
 if [ "$1" = "macos" ]; then
   mkdir -p SoraUnitySdkSamples/Assets/Plugins/SoraUnitySdk/macos
   rm -rf SoraUnitySdkSamples/Assets/Plugins/SoraUnitySdk/macos/SoraUnitySdk.bundle
   cp -r ../sora-unity-sdk/build/SoraUnitySdk.bundle SoraUnitySdkSamples/Assets/Plugins/SoraUnitySdk/macos/
+elif [ "$1" = "ios" ]; then
+  mkdir -p SoraUnitySdkSamples/Assets/Plugins/SoraUnitySdk/ios
+  rm -rf SoraUnitySdkSamples/Assets/Plugins/SoraUnitySdk/ios/libSoraUnitySdk.a
+  cp -r ../sora-unity-sdk/build/ios/_install/lib/libSoraUnitySdk.a SoraUnitySdkSamples/Assets/Plugins/SoraUnitySdk/ios/
+  cp -r ../sora-unity-sdk/_install/ios/webrtc/lib/libwebrtc.a SoraUnitySdkSamples/Assets/Plugins/SoraUnitySdk/ios/
 elif [ "$1" = "android" ]; then
   mkdir -p SoraUnitySdkSamples/Assets/Plugins/SoraUnitySdk/android/arm64-v8a/
   rm -f SoraUnitySdkSamples/Assets/Plugins/SoraUnitySdk/android/arm64-v8a/libSoraUnitySdk.so
   cp ../sora-unity-sdk/build/android/libSoraUnitySdk.so SoraUnitySdkSamples/Assets/Plugins/SoraUnitySdk/android/arm64-v8a/
   cp ../sora-unity-sdk/_install/android/webrtc/jar/webrtc.jar SoraUnitySdkSamples/Assets/Plugins/SoraUnitySdk/android/
 else
-  echo "$0 <macos|android>"
+  echo "$0 <macos|ios|android>"
   exit 1
 fi
