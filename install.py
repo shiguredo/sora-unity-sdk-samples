0d91a65009236721a8f7bd026dc8478c0cfb66f7
torikizikaeru <51085972+torikizi@users.noreply.github.com>
2024-10-03 15:44:19 +0900

    Sora Unity SDK を 2024.5.0-canary.4 に上げる

--- install.py
+++ install.py
@@ -12,7 +12,7 @@ from typing import Optional
 logging.basicConfig(level=logging.DEBUG)
 
 
-SORA_UNITY_SDK_VERSION = "2024.5.0-canary.3"
+SORA_UNITY_SDK_VERSION = "2024.5.0-canary.4"
 
 
 class ChangeDirectory(object):
